d7565198ed013113d7010fe43e5e5bf5a38988d9
tanaka-shigenori <s.tanaka0917@gmail.com>
2020-03-02 18:02:36 +0900

    パラメータが不正時のエラーメッセージを追加

--- delete.php
+++ delete.php
@@ -18,16 +18,31 @@
             ｜
             <a href="index.php">メモ一覧に戻る</a>
         </div>
+        <!-- ここから idがNULLのときエラーメッセージとして返す -->
+        <?php
+            if(isset($_REQUEST['id']) && is_numeric($_REQUEST['id'])){
+                //空欄や文字列をはじく。数字はOK
+                $id = $_REQUEST['id'];
 
-        <?php if(isset($_REQUEST['id']) && is_numeric($_REQUEST['id'])){
+                $memos = $db->prepare('SELECT * FROM memo WHERE id=?');
+                $memos->execute(array($id));
+                $memo = $memos->fetch();
+            }
+        ?>
+        <?php if(is_null($memo['id'])): ?>
+            <p class="error_message"> <?php echo "メモが存在しません"; exit(); ?> </p>
+        <?php endif ?>
+        <!-- ここまで idがNULLのときエラーメッセージとして返す -->
+        
+        <!-- ここから メモの削除 -->
+        <?php 
+            if(isset($_REQUEST['id']) && is_numeric($_REQUEST['id'])){
             $id = $_REQUEST['id'];
             $statement = $db->prepare('DELETE FROM memo WHERE id=?');
             $statement->execute(array($id));
         }
         ?>
-
-        
         <p class="delete_message">メモを削除しました</p>
-        
+        <!-- ここまで メモの削除 -->
     </body>
 </html>
