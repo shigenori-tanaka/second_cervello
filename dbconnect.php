e80910d3d8044bc8c8e7dfeeecee90956a512edd
tanaka-shigenori <s.tanaka0917@gmail.com>
2020-03-04 15:53:19 +0900

    ポスグレ接続2

--- dbconnect.php
+++ dbconnect.php
@@ -6,10 +6,26 @@
 //     echo 'DB接続エラー:'.$e->getMessage();
 // }
 
-$url = parse_url(getenv('DATABASE_URL'));
+// $url = parse_url(getenv('DATABASE_URL'));
 
-$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['ec2-52-207-93-32.compute-1.amazonaws.com'], substr($url['dftv124odntdma'], 1));   
+// $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['ec2-52-207-93-32.compute-1.amazonaws.com'], substr($url['dftv124odntdma'], 1));   
 
-$pdo = new PDO($dsn, $url['onvpenrlfwvnpw'], $url['a88a99591112ba605b2b18629e8769fda7003c9f394e0ee185339fa1008909f2']);
-var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+// $pdo = new PDO($dsn, $url['onvpenrlfwvnpw'], $url['a88a99591112ba605b2b18629e8769fda7003c9f394e0ee185339fa1008909f2']);
+// var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+
+function dbConnect(){
+    $db = parse_url($_SERVER['DATABASE_URL']);
+    $db['dftv124odntdma'] = ltrim($db['dftv124odntdma'], '/');
+    $dsn = "mysql:host={$db['ec2-52-207-93-32.compute-1.amazonaws.com']};dbname={$db['dftv124odntdma']};charset=utf8";
+    $user = $db['onvpenrlfwvnpw'];
+    $password = $db['a88a99591112ba605b2b18629e8769fda7003c9f394e0ee185339fa1008909f2'];
+    $options = array(
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+    );
+    $dbh = new PDO($dsn,$user,$password,$options);
+    return $dbh;
+  }
+
 ?>
