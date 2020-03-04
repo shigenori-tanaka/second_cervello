c46383385c94a48499f7563cb9b08f2e2965bb48
tanaka-shigenori <s.tanaka0917@gmail.com>
2020-03-04 14:21:54 +0900

    ポスグレ設定

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 try{
-    $DSN = 'mysql:dbname=second_cervello; host=localhost; charset=utf8';
-    $db = new PDO($DSN,'root','root');
+    $DSN = 'mysql:dbname=dftv124odntdma; host=ec2-52-207-93-32.compute-1.amazonaws.com; charset=utf8';
+    $db = new PDO($DSN,'onvpenrlfwvnpw','a88a99591112ba605b2b18629e8769fda7003c9f394e0ee185339fa1008909f2');
 }   catch(PDOException $e){
     echo 'DB接続エラー:'.$e->getMessage();
 }
