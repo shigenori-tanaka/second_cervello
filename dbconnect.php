b4d76b50da0a8ffa839f4d5a5cdd914804b66cbb
tanaka-shigenori <s.tanaka0917@gmail.com>
2020-03-04 16:27:02 +0900

    見やすくしました。

--- dbconnect.php
+++ dbconnect.php
@@ -5,14 +5,15 @@
 // }   catch(PDOException $e){
 //     echo 'DB接続エラー:'.$e->getMessage();
 // }
+
 try{
-$url = parse_url(getenv('DATABASE_URL'));
+    $url = parse_url(getenv('DATABASE_URL'));
 
-$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+    $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
 
-$pdo = new PDO($dsn, $url['user'], $url['pass']);
-// var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
-}   catch(PDOException $e){
-        echo 'DB接続エラー:'.$e->getMessage();
-    }
+    $pdo = new PDO($dsn, $url['user'], $url['pass']);
+    // var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+}catch(PDOException $e){
+    echo 'DB接続エラー:'.$e->getMessage();
+}
 ?>
