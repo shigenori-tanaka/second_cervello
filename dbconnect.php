0ddc8fd48f00cd356e28881296061707d48f48af
tanaka-shigenori <s.tanaka0917@gmail.com>
2020-03-04 22:07:14 +0900

    いらない部分を削除

--- dbconnect.php
+++ dbconnect.php
@@ -8,12 +8,10 @@
 
 try{
     $url = parse_url(getenv('DATABASE_URL'));
-
     $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
-
     $pdo = new PDO($dsn, $url['user'], $url['pass']);
-    // var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
 }catch(PDOException $e){
     echo 'DB接続エラー:'.$e->getMessage();
 }
+
 ?>
