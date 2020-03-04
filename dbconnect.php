a91b90a802685c933796e84be70a3ff34fd3a132
tanaka-shigenori <s.tanaka0917@gmail.com>
2020-03-04 22:14:03 +0900

    追加UP

--- dbconnect.php
+++ dbconnect.php
@@ -1,17 +1,17 @@
 <?php
-// try{
-//     $DSN = 'mysql:dbname=second_cervello; host=localhost; charset=utf8';
-//     $db = new PDO($DSN,'root','root');
-// }   catch(PDOException $e){
-//     echo 'DB接続エラー:'.$e->getMessage();
-// }
-
 try{
-    $url = parse_url(getenv('DATABASE_URL'));
-    $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
-    $pdo = new PDO($dsn, $url['user'], $url['pass']);
-}catch(PDOException $e){
+    $DSN = 'mysql:dbname=second_cervello; host=localhost; charset=utf8';
+    $db = new PDO($DSN,'root','root');
+}   catch(PDOException $e){
     echo 'DB接続エラー:'.$e->getMessage();
 }
+
+// try{
+//     $url = parse_url(getenv('DATABASE_URL'));
+//     $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+//     $pdo = new PDO($dsn, $url['user'], $url['pass']);
+// }catch(PDOException $e){
+//     echo 'DB接続エラー:'.$e->getMessage();
+// }
 
 ?>
